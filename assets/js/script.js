64757544f09b92851789762b4e92ce5bc55e1434
AnthonyR <arankin789@gmail.com>
2022-04-08 13:14:12 -0400

    pulls and displays 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,29 @@
 // var searchEl = document.querySelector("#search");
+
+var cities = [];
 const apiKey = "78226ef2dd12503663f82f2dfa7771bc";
 
 var searchEl = document.querySelector("#citySearch");
 var weatherContainerEl = document.querySelector("#currentWeatherContainer");
 var searchedCityEl = document.querySelector("#currentCity");
+var forecastContainerEl = document.querySelector("#forecastFiveDay");
+
 
 var searchHandler = function(event){
     event.preventDefault();
     var city = searchEl.value.trim();
-    console.log(city);
-    console.log("clicked search button")
 
+    if(city){
+        getWeather(city);
+        getForecast(city);
 
-    getWeather(city);
+        cities.unshift({city});
+        searchEl.value = "";
+    }
+    else {
+        alert("Please enter a City");
+    }
+
 };
 
 var saveSearch = function(){
@@ -32,16 +43,16 @@ var getWeather = function(city){
 };
 
 // displays current weather
-var displayWeather = function(weather){
-    console.log(weather);
+var displayWeather = function(weather, searchCity){
+    // console.log(weather);
 
     // clear old content
     weatherContainerEl.textContent = "";
-    searchEl.textContent = "";
+    searchedCityEl.textContent = "";
 
     // create data element
     var currentDate = document.createElement("span");
-    currentDate.textContent=" (" + moment(weather.value).format("MMM D, YYYY") + ") ";
+    currentDate.textContent= searchCity + " (" + moment(weather.value).format("MMM D, YYYY") + ") ";
     searchedCityEl.appendChild(currentDate);
 
     // create image element
@@ -84,7 +95,7 @@ var getUv = function(lat, lon){
     .then(function(response){
         response.json().then(function(data){
             displayUv(data);
-            console.log(data);
+            // console.log(data);
         });
     });
 }
@@ -111,5 +122,62 @@ var displayUv = function(index){
     displayUvEl.appendChild(uvIndex);
     weatherContainerEl.appendChild(displayUvEl);
 }
+
+// pulls 5 day forecast 
+
+var getForecast = function(city){
+    var apiURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=' + apiKey;
+
+    fetch(apiURL)
+    .then(function(response){
+        response.json().then(function(data){
+            displayForecast(data)
+            console.log(data)
+        });
+    });
+};
+
+// displays 5 day forecast
+var displayForecast = function(weather){
+    forecastContainerEl.textContent = "";
+
+    var forecast = weather.list;
+    
+    for(var i=5; i <forecast.length; i= i+8){
+        var dailyForecast = forecast[i];
+
+        // create div for each day
+        var forecastEl = document.createElement("div");
+        forecastEl.classList = "card bg-primary text-light m-2";
+
+        var forecastDate = document.createElement("h4");
+        forecastDate.textContent = moment.unix(dailyForecast.dt).format("MMM D, YYYY");
+        forecastDate.classList = "card-header text-center";
+        forecastEl.appendChild(forecastDate);
+
+        console.log(dailyForecast);
+        
+
+        var weatherIcon = document.createElement("img");
+        weatherIcon.classList = "card-body text-center";
+        // weatherIcon.setAttribute("src", 'https://openweathermap.org/img/wn/${dailyForecast.weather[0].icon}@2x.png');
+        forecastEl.appendChild(weatherIcon);
+
+        var forecastTempEl = document.createElement("span");
+        forecastTempEl.classList = "card-body text-center";
+        forecastTempEl.textContent = dailyForecast.main.temp + " °F";
+        forecastEl.appendChild(forecastTempEl);
+        
+        var forecastHumidEl = document.createElement("span");
+        forecastHumidEl.classList = "card-body text-center";
+        forecastHumidEl.textContent = dailyForecast.main.humidity + " %";        
+        
+        forecastEl.appendChild(forecastHumidEl);
+
+        forecastContainerEl.appendChild(forecastEl);
+    }
+    
+}
+
 // click handlers
 $("#searchBtn").on("click", searchHandler);
